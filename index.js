d9837f79ae4a45837c9aa86f734d8bf500d17431
Garrett <garrettjrh@live.com>
2023-01-09 08:51:16 -0700

    Fixed an error that showed all of the users cards in one column instead of two.

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ let users;
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
-    const userCard = document.querySelector(".user")
+    const userList = document.querySelector(".user-list")
     
     const usersHTML = usersData.map((user) => {
-      return `<div class="user-card">
-      <div class="user-card__container">
-        <h3>${user.name}</h3>
-          <p><b>Email:</b> ${user.email}</p>
-          <p><b>Phone:</b> ${user.phone}</p>
-          <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+      return `<div class="user">
+      <div class="user-card">
+        <div class="user-card__container">
+          <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+        </div>
       </div>
     </div>`
     }).join('');
     console.log(usersData)
-    userCard.innerHTML = usersHTML
+    userList.innerHTML = usersHTML
 }
 
 main();
