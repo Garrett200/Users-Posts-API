bc57107a66cb8ec9b9fe82797d642bc77b6d3dc7
Garrett <garrettjrh@live.com>
2023-01-09 08:46:54 -0700

    Finished the javascript function to show all users names, phone #'s, emails, and websites.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+let users;
+
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+    const userCard = document.querySelector(".user")
+    
+    const usersHTML = usersData.map((user) => {
+      return `<div class="user-card">
+      <div class="user-card__container">
+        <h3>${user.name}</h3>
+          <p><b>Email:</b> ${user.email}</p>
+          <p><b>Phone:</b> ${user.phone}</p>
+          <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+      </div>
+    </div>`
+    }).join('');
+    console.log(usersData)
+    userCard.innerHTML = usersHTML
+}
+
+main();
