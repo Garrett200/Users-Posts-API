6a73149539565b1bf85d211bd17e4f3513cb922c
Garrett <garrettjrh@live.com>
2023-01-10 08:14:10 -0700

    Finished the java to display each users posts.

--- user.js
+++ user.js
@@ -1,5 +1,21 @@
+const postList = document.querySelector(".post-list");
 
-function userPosts() {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersData = await users.json();
+async function main() {
+    const id = localStorage.getItem("id");
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+    const postsData = await posts.json();
+    console.log(postsData)
+    postList.innerHTML = postsData.map(post => usersPosts(post)).join('');
+}
+main();
+
+function usersPosts(post) {
+    return `<div class="post">
+        <div class="post__title">
+            ${post.title}
+        </div>
+        <p class="post__body">
+            ${post.body}
+        </p>
+    </div>`
 }
