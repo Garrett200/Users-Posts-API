70adf4cd2588dcaefddb0453d6fd8781fccd6d43
Garrett <garrettjrh@live.com>
2023-01-10 08:28:59 -0700

    Finished search function by id for user posts.

--- user.js
+++ user.js
@@ -1,15 +1,19 @@
 const postList = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
-async function main() {
-    const id = localStorage.getItem("id");
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
+}
+
+async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
     const postsData = await posts.json();
-    console.log(postsData)
-    postList.innerHTML = postsData.map(post => usersPosts(post)).join('');
-}
-main();
 
-function usersPosts(post) {
+    postList.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
+
+function postHTML(post) {
     return `<div class="post">
         <div class="post__title">
             ${post.title}
@@ -19,3 +23,5 @@ function usersPosts(post) {
         </p>
     </div>`
 }
+
+renderPosts(id);
